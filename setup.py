47f80dd8b9fbe436d2a457ae38e8a96cb26b160e
Jake Howard <jake.howard@dabapps.com>
2020-04-20 16:38:58 +0100

    Don't read requirements at build time
    List them in `setup.py` itself, it's easier!

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup
 with open("README.md") as f:
     readme = f.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().split("\n")
-
 setup(
     name="crabtools",
     version=__import__("crab").__version__,
@@ -16,6 +13,11 @@ setup(
     long_description_content_type="text/markdown",
     license="BSD",
     packages=["crab"],
-    install_requires=requirements,
+    install_requires=[
+        "Flask==1.0.3",
+        "psutil==5.6.3",
+        "requests==2.22.0",
+        "python-dotenv==0.10.3",
+    ],
     entry_points={"console_scripts": ["crab=crab.cli:main"]},
 )
