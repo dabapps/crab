5c4883b1122959233ea79c83005801e009309783
Jake Howard <jake.howard@dabapps.com>
2020-04-30 15:56:04 +0100

    Run black over tests

--- tests/test_router.py
+++ tests/test_router.py
@@ -17,7 +17,11 @@ class RouterTestCase(TestCase):
     def test_multiple_routes(self):
         with subprocess.Popen(
             ["sleep", "5"],
-            env={**os.environ, "VIRTUAL_HOST": "test.localhost,other-test.localhost", "PORT": "1234"},
+            env={
+                **os.environ,
+                "VIRTUAL_HOST": "test.localhost,other-test.localhost",
+                "PORT": "1234",
+            },
         ) as subproc:
             self.assertEqual(get_route_for_hostname("test.localhost"), "1234")
             self.assertEqual(get_route_for_hostname("other-test.localhost"), "1234")
